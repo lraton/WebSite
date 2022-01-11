d4cf1a08e94fee96ac8a048f90ffe2baa146c619
lraton <50119007+lraton@users.noreply.github.com>
2022-01-11 23:50:03 +0100

    add scrollbar

--- script/script.js
+++ script/script.js
@@ -1,15 +1,18 @@
-function reveal(  ) {
+function reveal( ) {
   var reveals = document.querySelectorAll(".reveal");
-
+  const mediaQuery = window.matchMedia('(max-width: 1300px)');
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
     var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
+    if(mediaQuery.matches) {
       reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
+    }else{
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
     }
   }
 }
