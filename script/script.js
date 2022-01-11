12508120c3a629dbe884af5f28765ab3f6e525ad
lraton <50119007+lraton@users.noreply.github.com>
2022-01-11 21:44:23 +0100

    modified load script

--- script/script.js
+++ script/script.js
@@ -14,4 +14,25 @@ function reveal() {
   }
 }
 
-window.addEventListener("scroll", reveal);
+function reveal2() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    reveals[i].classList.add("active");
+  }
+}
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    window.addEventListener("scroll", reveal2);
+  } else {
+    window.addEventListener("scroll", reveal);
+  }
+}
+
+var x = window.matchMedia("(max-width: 1300px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
