6e40cec37d014a77fea924225e96e5913ceefa10
lraton <50119007+lraton@users.noreply.github.com>
2022-01-11 21:45:37 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -18,9 +18,6 @@ function reveal2() {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
     reveals[i].classList.add("active");
   }
 }
