3e099ea169f0844bfbb58ea02c10f666ec371f6d
lraton <50119007+lraton@users.noreply.github.com>
2022-01-11 21:59:44 +0100

    footer and script

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function reveal() {
+function reveal(  ) {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
@@ -13,23 +13,4 @@ function reveal() {
     }
   }
 }
-
-function reveal2() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    reveals[i].classList.add("active");
-  }
-}
-
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    window.addEventListener("scroll", reveal2);
-  } else {
-    window.addEventListener("scroll", reveal);
-  }
-}
-
-var x = window.matchMedia("(max-width: 1300px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+window.addEventListener("scroll", reveal);
